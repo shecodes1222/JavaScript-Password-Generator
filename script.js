d3b4edd310c8d077f68c2ea472b9a8d67683a142
Jasmina <122895875+shecodes1222@users.noreply.github.com>
2023-06-26 10:07:25 -0400

    added function to generate pw

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ var capitalCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 var lowerCase = ["a", "b", "c", "d", "e","f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "#", "$", "&", "(",")", "*", "+", "-", ".","/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+
+// Function to generate password
+function generatePassword() {
+    var text = "";
+    var consolidatedArr = [];
+
+      //Getting input from the user
+  let passLength = prompt("Enter the length of your password between 8-128 characters.");
